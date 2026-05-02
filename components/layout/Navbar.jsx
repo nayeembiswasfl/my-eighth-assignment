5e504af57570a96c9dfa87c199a927cee12401d7
nayeembiswasfl <nayeembiswasfl@gmail.com>
2026-05-02 11:40:24 +0600

    fix final logo path

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar({ dark = false }) {
   return (
     <header className={`site-header ${dark ? "site-header-dark" : ""}`}>
       <Link href="/" className="brand" aria-label="TileCraft home">
-        <Image src="/images/brand/website-logo.png" alt="TileCraft" width={68} height={65} priority />
+        <Image src="/images/brand/final-logo.png" alt="TileCraft" width={68} height={65} priority />
       </Link>
 
       <nav className={`nav-links ${open ? "is-open" : ""}`}>
